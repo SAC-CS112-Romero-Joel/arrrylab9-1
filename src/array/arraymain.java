a56494b209edb7efc2c305de29b4a4041ed2592f
SAC-CS112-Romero-Joel <Joelasap101@gmail.com>
2015-10-21 19:32:58 -0700

    lab9-1

---
+++ src/array/arraymain.java
@@ -0,0 +1,27 @@
+package array;
+import java.util.Arrays;
+public class arraymain {
+
+	public static void main(String[] args) {
+		int [] aryNums;
+		int sum = 0, average;
+		aryNums = new int [6];
+		
+		aryNums [0] = 123;
+		aryNums [1] = 456;
+		aryNums [2] = 479;
+		aryNums [3] = 135;
+		aryNums [4] = 246;
+		aryNums [5] = 135;
+		
+		Arrays.sort(aryNums);
+		
+		sum = ( aryNums[1]+aryNums[2]+aryNums[3]+aryNums[4]+aryNums[0]+aryNums[5]);
+		average = (sum / 5);
+		
+		System.out.println("The Average is :"+average);
+		
+		
+	}
+
+}
